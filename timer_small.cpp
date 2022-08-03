46dc0c58e8a32849ef431940a06d4d3d33e170ac
Simon-Bird-Code <109514172+Simon-Bird-Code@users.noreply.github.com>
2022-08-03 12:02:43 +0100

    Add files via upload

---
+++ timer_small.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<time.h>
+#define m printf("\a")
+#define t time(0)
+main()
+{
+    int w,r,i=t,e=r=7,o=w=3;
+    while(e)
+        {
+            if(t==i)
+                {
+                    m;e--;w=r+o-w;i+=(r<<o)*w;
+                }
+        }
+}
+
